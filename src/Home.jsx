ad8473c7ea741ac0a415ae9b0aa46ff75113858c
sshowmik <sadman.showmik@g.bracu.ac.bd>
2022-09-11 11:46:02 +0600

    11 Sept 12 pm

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <HeroSection/>
+    
+  
 }
 
 export default Home
